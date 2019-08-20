b7ce13c1ce1223fa0f9a3e9e778e3484537220c0
Federico Feroldi <pix@yahoo.it>
2019-08-20 15:58:23 +0200

    Adds GetMessage handler

---
+++ GetMessage/__tests__/handler.test.ts
@@ -0,0 +1,468 @@
+/* tslint:disable:no-any */
+/* tslint:disable:no-duplicate-string */
+/* tslint:disable:no-big-function */
+
+jest.mock("winston");
+
+import { Either, left, right } from "fp-ts/lib/Either";
+import { none, Option, some } from "fp-ts/lib/Option";
+
+import { QueryError } from "documentdb";
+
+import {
+  IAzureApiAuthorization,
+  UserGroup
+} from "io-functions-commons/dist/src/utils/middlewares/azure_api_auth";
+import { IAzureUserAttributes } from "io-functions-commons/dist/src/utils/middlewares/azure_user_attributes";
+
+import { NonNegativeNumber } from "italia-ts-commons/lib/numbers";
+import {
+  EmailString,
+  FiscalCode,
+  NonEmptyString,
+  OrganizationFiscalCode
+} from "italia-ts-commons/lib/strings";
+
+import {
+  NewMessageWithoutContent,
+  RetrievedMessageWithoutContent
+} from "io-functions-commons/dist/src/models/message";
+import { MessageStatus } from "io-functions-commons/dist/src/models/message_status";
+import {
+  NotificationAddressSourceEnum,
+  RetrievedNotification
+} from "io-functions-commons/dist/src/models/notification";
+import {
+  makeStatusId,
+  RetrievedNotificationStatus
+} from "io-functions-commons/dist/src/models/notification_status";
+import { toAuthorizedCIDRs } from "io-functions-commons/dist/src/models/service";
+
+import { CreatedMessageWithoutContent } from "io-functions-commons/dist/generated/definitions/CreatedMessageWithoutContent";
+import { MaxAllowedPaymentAmount } from "io-functions-commons/dist/generated/definitions/MaxAllowedPaymentAmount";
+import { MessageResponseWithoutContent } from "io-functions-commons/dist/generated/definitions/MessageResponseWithoutContent";
+import { MessageStatusValueEnum } from "io-functions-commons/dist/generated/definitions/MessageStatusValue";
+import { NotificationChannelEnum } from "io-functions-commons/dist/generated/definitions/NotificationChannel";
+import { NotificationChannelStatusValueEnum } from "io-functions-commons/dist/generated/definitions/NotificationChannelStatusValue";
+import { ServiceId } from "io-functions-commons/dist/generated/definitions/ServiceId";
+import { TimeToLiveSeconds } from "io-functions-commons/dist/generated/definitions/TimeToLiveSeconds";
+
+import { GetMessageHandler } from "../handler";
+
+afterEach(() => {
+  jest.resetAllMocks();
+  jest.restoreAllMocks();
+});
+
+const aFiscalCode = "FRLFRC74E04B157I" as FiscalCode;
+const anOrganizationFiscalCode = "01234567890" as OrganizationFiscalCode;
+const anEmail = "test@example.com" as EmailString;
+
+const someUserAttributes: IAzureUserAttributes = {
+  email: anEmail,
+  kind: "IAzureUserAttributes",
+  service: {
+    authorizedCIDRs: toAuthorizedCIDRs([]),
+    authorizedRecipients: new Set([]),
+    departmentName: "IT" as NonEmptyString,
+    isVisible: true,
+    maxAllowedPaymentAmount: 0 as MaxAllowedPaymentAmount,
+    organizationFiscalCode: anOrganizationFiscalCode,
+    organizationName: "AgID" as NonEmptyString,
+    serviceId: "test" as NonEmptyString,
+    serviceName: "Test" as NonEmptyString,
+    version: 1 as NonNegativeNumber
+  }
+};
+
+const aUserAuthenticationDeveloper: IAzureApiAuthorization = {
+  groups: new Set([UserGroup.ApiMessageRead, UserGroup.ApiMessageWrite]),
+  kind: "IAzureApiAuthorization",
+  subscriptionId: "s123" as NonEmptyString,
+  userId: "u123" as NonEmptyString
+};
+
+const aUserAuthenticationTrustedApplication: IAzureApiAuthorization = {
+  groups: new Set([UserGroup.ApiMessageRead, UserGroup.ApiMessageList]),
+  kind: "IAzureApiAuthorization",
+  subscriptionId: "s123" as NonEmptyString,
+  userId: "u123" as NonEmptyString
+};
+
+const aMessageId = "A_MESSAGE_ID" as NonEmptyString;
+
+const aNewMessageWithoutContent: NewMessageWithoutContent = {
+  createdAt: new Date(),
+  fiscalCode: aFiscalCode,
+  id: "A_MESSAGE_ID" as NonEmptyString,
+  indexedId: "A_MESSAGE_ID" as NonEmptyString,
+  isPending: true,
+  kind: "INewMessageWithoutContent",
+  senderServiceId: "test" as ServiceId,
+  senderUserId: "u123" as NonEmptyString,
+  timeToLiveSeconds: 3600 as TimeToLiveSeconds
+};
+
+const aRetrievedMessageWithoutContent: RetrievedMessageWithoutContent = {
+  ...aNewMessageWithoutContent,
+  _self: "xyz",
+  _ts: 1,
+  kind: "IRetrievedMessageWithoutContent"
+};
+
+const aPublicExtendedMessage: CreatedMessageWithoutContent = {
+  created_at: new Date(),
+  fiscal_code: aNewMessageWithoutContent.fiscalCode,
+  id: "A_MESSAGE_ID",
+  sender_service_id: aNewMessageWithoutContent.senderServiceId
+};
+
+const aPublicExtendedMessageResponse: MessageResponseWithoutContent = {
+  message: aPublicExtendedMessage,
+  notification: {
+    email: NotificationChannelStatusValueEnum.SENT,
+    webhook: NotificationChannelStatusValueEnum.SENT
+  },
+  status: MessageStatusValueEnum.ACCEPTED
+};
+
+function getNotificationModelMock(
+  aRetrievedNotification: any = { data: "data" }
+): any {
+  return {
+    findNotificationForMessage: jest.fn(() =>
+      Promise.resolve(right(some(aRetrievedNotification)))
+    )
+  };
+}
+
+const aRetrievedNotificationStatus: RetrievedNotificationStatus = {
+  _self: "xyz",
+  _ts: 123,
+  channel: NotificationChannelEnum.EMAIL,
+  id: "1" as NonEmptyString,
+  kind: "IRetrievedNotificationStatus",
+  messageId: "1" as NonEmptyString,
+  notificationId: "1" as NonEmptyString,
+  status: NotificationChannelStatusValueEnum.SENT,
+  statusId: makeStatusId("1" as NonEmptyString, NotificationChannelEnum.EMAIL),
+  updatedAt: new Date(),
+  version: 1 as NonNegativeNumber
+};
+
+const aMessageStatus: MessageStatus = {
+  messageId: aMessageId,
+  status: MessageStatusValueEnum.ACCEPTED,
+  updatedAt: new Date()
+};
+
+function getNotificationStatusModelMock(
+  retrievedNotificationStatus: any = right(some(aRetrievedNotificationStatus))
+): any {
+  return {
+    findOneNotificationStatusByNotificationChannel: jest.fn(() =>
+      Promise.resolve(retrievedNotificationStatus)
+    )
+  };
+}
+
+function getMessageStatusModelMock(
+  s: Either<QueryError, Option<MessageStatus>> = right(some(aMessageStatus))
+): any {
+  return {
+    findOneByMessageId: jest.fn().mockReturnValue(Promise.resolve(s)),
+    upsert: jest.fn(status => Promise.resolve(left(status)))
+  };
+}
+
+describe("GetMessageHandler", () => {
+  it("should respond with a message if requesting user is the sender", async () => {
+    const mockMessageModel = {
+      findMessageForRecipient: jest.fn(() =>
+        right(some(aRetrievedMessageWithoutContent))
+      ),
+      getStoredContent: jest.fn(() => right(none))
+    };
+
+    const getMessageHandler = GetMessageHandler(
+      mockMessageModel as any,
+      getMessageStatusModelMock(),
+      getNotificationModelMock(),
+      getNotificationStatusModelMock(),
+      {} as any
+    );
+
+    const result = await getMessageHandler(
+      aUserAuthenticationDeveloper,
+      undefined as any, // not used
+      someUserAttributes,
+      aFiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(mockMessageModel.getStoredContent).toHaveBeenCalledTimes(1);
+    expect(mockMessageModel.findMessageForRecipient).toHaveBeenCalledTimes(1);
+    expect(mockMessageModel.findMessageForRecipient).toHaveBeenCalledWith(
+      aRetrievedMessageWithoutContent.fiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(result.kind).toBe("IResponseSuccessJson");
+    if (result.kind === "IResponseSuccessJson") {
+      expect(result.value).toEqual(aPublicExtendedMessageResponse);
+    }
+  });
+
+  it("should fail if any error occurs trying to retrieve the message content", async () => {
+    const mockMessageModel = {
+      findMessageForRecipient: jest.fn(() =>
+        right(some(aRetrievedMessageWithoutContent))
+      ),
+      getStoredContent: jest.fn(() => left(new Error()))
+    };
+
+    const getMessageHandler = GetMessageHandler(
+      mockMessageModel as any,
+      getMessageStatusModelMock(),
+      getNotificationModelMock(),
+      getNotificationStatusModelMock(),
+      {} as any
+    );
+
+    const result = await getMessageHandler(
+      aUserAuthenticationDeveloper,
+      undefined as any, // not used
+      someUserAttributes,
+      aFiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(mockMessageModel.getStoredContent).toHaveBeenCalledTimes(1);
+    expect(mockMessageModel.findMessageForRecipient).toHaveBeenCalledTimes(1);
+    expect(mockMessageModel.findMessageForRecipient).toHaveBeenCalledWith(
+      aRetrievedMessageWithoutContent.fiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(result.kind).toBe("IResponseErrorInternal");
+  });
+
+  it("should respond with a message if requesting user is a trusted application", async () => {
+    const mockMessageModel = {
+      findMessageForRecipient: jest.fn(() =>
+        right(some(aRetrievedMessageWithoutContent))
+      ),
+      getStoredContent: jest.fn(() => right(none))
+    };
+
+    const getMessageHandler = GetMessageHandler(
+      mockMessageModel as any,
+      getMessageStatusModelMock(),
+      getNotificationModelMock(),
+      getNotificationStatusModelMock(),
+      {} as any
+    );
+
+    const result = await getMessageHandler(
+      aUserAuthenticationTrustedApplication,
+      undefined as any, // not used
+      someUserAttributes,
+      aFiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(mockMessageModel.getStoredContent).toHaveBeenCalledTimes(1);
+    expect(mockMessageModel.findMessageForRecipient).toHaveBeenCalledTimes(1);
+    expect(mockMessageModel.findMessageForRecipient).toHaveBeenCalledWith(
+      aRetrievedMessageWithoutContent.fiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(result.kind).toBe("IResponseSuccessJson");
+    if (result.kind === "IResponseSuccessJson") {
+      expect(result.value).toEqual(aPublicExtendedMessageResponse);
+    }
+  });
+
+  it("should respond with forbidden if requesting user is not the sender", async () => {
+    const message = {
+      ...aRetrievedMessageWithoutContent,
+      senderServiceId: "anotherOrg"
+    };
+
+    const mockMessageModel = {
+      findMessageForRecipient: jest.fn(() => right(some(message))),
+      getStoredContent: jest.fn(() => right(none))
+    };
+
+    const getMessageHandler = GetMessageHandler(
+      mockMessageModel as any,
+      getMessageStatusModelMock(),
+      {} as any,
+      {} as any,
+      {} as any
+    );
+
+    const result = await getMessageHandler(
+      aUserAuthenticationDeveloper,
+      undefined as any, // not used
+      someUserAttributes,
+      aFiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(mockMessageModel.findMessageForRecipient).toHaveBeenCalledTimes(1);
+    expect(mockMessageModel.findMessageForRecipient).toHaveBeenCalledWith(
+      aRetrievedMessageWithoutContent.fiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(result.kind).toBe("IResponseErrorForbiddenNotAuthorized");
+  });
+
+  it("should respond with not found a message doesn not exist", async () => {
+    const mockMessageModel = {
+      findMessageForRecipient: jest.fn(() => right(none)),
+      getStoredContent: jest.fn(() => right(none))
+    };
+
+    const getMessageHandler = GetMessageHandler(
+      mockMessageModel as any,
+      getMessageStatusModelMock(),
+      {} as any,
+      {} as any,
+      {} as any
+    );
+
+    const result = await getMessageHandler(
+      aUserAuthenticationDeveloper,
+      undefined as any, // not used
+      someUserAttributes,
+      aFiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(mockMessageModel.findMessageForRecipient).toHaveBeenCalledTimes(1);
+
+    expect(result.kind).toBe("IResponseErrorNotFound");
+  });
+
+  it("should provide information about notification status", async () => {
+    const aRetrievedNotification: RetrievedNotification = {
+      _self: "xyz",
+      _ts: 123,
+      channels: {
+        [NotificationChannelEnum.EMAIL]: {
+          addressSource: NotificationAddressSourceEnum.PROFILE_ADDRESS,
+          toAddress: "x@example.com" as EmailString
+        }
+      },
+      fiscalCode: aFiscalCode,
+      id: "A_NOTIFICATION_ID" as NonEmptyString,
+      kind: "IRetrievedNotification",
+      messageId: "A_MESSAGE_ID" as NonEmptyString
+    };
+
+    const mockMessageModel = {
+      findMessageForRecipient: jest.fn(() =>
+        right(some(aRetrievedMessageWithoutContent))
+      ),
+      getStoredContent: jest.fn(() => right(none))
+    };
+
+    const getMessageHandler = GetMessageHandler(
+      mockMessageModel as any,
+      getMessageStatusModelMock(),
+      getNotificationModelMock(aRetrievedNotification),
+      getNotificationStatusModelMock(),
+      {} as any
+    );
+
+    const result = await getMessageHandler(
+      aUserAuthenticationTrustedApplication,
+      undefined as any, // not used
+      someUserAttributes,
+      aFiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(mockMessageModel.getStoredContent).toHaveBeenCalledTimes(1);
+    expect(mockMessageModel.findMessageForRecipient).toHaveBeenCalledTimes(1);
+    expect(mockMessageModel.findMessageForRecipient).toHaveBeenCalledWith(
+      aRetrievedMessageWithoutContent.fiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(result.kind).toBe("IResponseSuccessJson");
+    if (result.kind === "IResponseSuccessJson") {
+      expect(result.value).toEqual(aPublicExtendedMessageResponse);
+    }
+  });
+
+  it("should fail if any error occurs trying to retrieve the message status", async () => {
+    const mockMessageModel = {
+      findMessageForRecipient: jest.fn(() =>
+        right(some(aRetrievedMessageWithoutContent))
+      ),
+      getStoredContent: jest.fn(() => right(none))
+    };
+
+    const getMessageHandler = GetMessageHandler(
+      mockMessageModel as any,
+      getMessageStatusModelMock(
+        left<QueryError, Option<MessageStatus>>({
+          body: "error",
+          code: 1
+        })
+      ),
+      getNotificationModelMock(),
+      getNotificationStatusModelMock(),
+      {} as any
+    );
+
+    const result = await getMessageHandler(
+      aUserAuthenticationDeveloper,
+      undefined as any, // not used
+      someUserAttributes,
+      aFiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(result.kind).toBe("IResponseErrorInternal");
+  });
+  it("should fail if any error occurs trying to retrieve the notification status", async () => {
+    const mockMessageModel = {
+      findMessageForRecipient: jest.fn(() =>
+        right(some(aRetrievedMessageWithoutContent))
+      ),
+      getStoredContent: jest.fn(() => right(none))
+    };
+
+    const getMessageHandler = GetMessageHandler(
+      mockMessageModel as any,
+      getMessageStatusModelMock(),
+      {
+        findNotificationForMessage: jest.fn(() =>
+          Promise.resolve(
+            left({
+              body: "error",
+              code: 1
+            })
+          )
+        )
+      } as any,
+      getNotificationStatusModelMock(),
+      {} as any
+    );
+
+    const result = await getMessageHandler(
+      aUserAuthenticationDeveloper,
+      undefined as any, // not used
+      someUserAttributes,
+      aFiscalCode,
+      aRetrievedMessageWithoutContent.id
+    );
+
+    expect(result.kind).toBe("IResponseErrorInternal");
+  });
+});
---
+++ GetMessage/handler.ts
@@ -0,0 +1,242 @@
+import * as express from "express";
+import * as winston from "winston";
+
+import {
+  ClientIp,
+  ClientIpMiddleware
+} from "io-functions-commons/dist/src/utils/middlewares/client_ip_middleware";
+
+import { RequiredParamMiddleware } from "io-functions-commons/dist/src/utils/middlewares/required_param";
+
+import {
+  IResponseErrorQuery,
+  ResponseErrorQuery
+} from "io-functions-commons/dist/src/utils/response";
+
+import {
+  AzureApiAuthMiddleware,
+  IAzureApiAuthorization,
+  UserGroup
+} from "io-functions-commons/dist/src/utils/middlewares/azure_api_auth";
+import {
+  AzureUserAttributesMiddleware,
+  IAzureUserAttributes
+} from "io-functions-commons/dist/src/utils/middlewares/azure_user_attributes";
+import { FiscalCodeMiddleware } from "io-functions-commons/dist/src/utils/middlewares/fiscalcode";
+import {
+  withRequestMiddlewares,
+  wrapRequestHandler
+} from "io-functions-commons/dist/src/utils/request_middleware";
+import {
+  IResponseErrorForbiddenNotAuthorized,
+  IResponseErrorInternal,
+  IResponseErrorNotFound,
+  IResponseErrorValidation,
+  IResponseSuccessJson,
+  ResponseErrorForbiddenNotAuthorized,
+  ResponseErrorInternal,
+  ResponseErrorNotFound,
+  ResponseSuccessJson
+} from "italia-ts-commons/lib/responses";
+import { FiscalCode, NonEmptyString } from "italia-ts-commons/lib/strings";
+
+import {
+  checkSourceIpForHandler,
+  clientIPAndCidrTuple as ipTuple
+} from "io-functions-commons/dist/src/utils/source_ip_check";
+
+import { NotificationModel } from "io-functions-commons/dist/src/models/notification";
+import { ServiceModel } from "io-functions-commons/dist/src/models/service";
+
+import { BlobService } from "azure-storage";
+
+import {
+  MessageModel,
+  RetrievedMessage
+} from "io-functions-commons/dist/src/models/message";
+
+import { isLeft } from "fp-ts/lib/Either";
+import { isNone } from "fp-ts/lib/Option";
+
+import { MessageStatusModel } from "io-functions-commons/dist/src/models/message_status";
+import { NotificationStatusModel } from "io-functions-commons/dist/src/models/notification_status";
+
+import { getMessageNotificationStatuses } from "io-functions-commons/dist/src/utils/messages";
+
+import { MessageResponseWithContent } from "io-functions-commons/dist/generated/definitions/MessageResponseWithContent";
+import { MessageResponseWithoutContent } from "io-functions-commons/dist/generated/definitions/MessageResponseWithoutContent";
+import { MessageStatusValueEnum } from "io-functions-commons/dist/generated/definitions/MessageStatusValue";
+
+/**
+ * Type of a GetMessage handler.
+ *
+ * GetMessage expects a FiscalCode and a Message ID as input
+ * and returns a Message as output or a Not Found or Validation
+ * errors.
+ */
+type IGetMessageHandler = (
+  auth: IAzureApiAuthorization,
+  clientIp: ClientIp,
+  attrs: IAzureUserAttributes,
+  fiscalCode: FiscalCode,
+  messageId: string
+) => Promise<
+  // tslint:disable-next-line:max-union-size
+  | IResponseSuccessJson<
+      MessageResponseWithContent | MessageResponseWithoutContent
+    >
+  | IResponseErrorNotFound
+  | IResponseErrorQuery
+  | IResponseErrorValidation
+  | IResponseErrorForbiddenNotAuthorized
+  | IResponseErrorInternal
+>;
+
+/**
+ * Converts a retrieved message to a message that can be shared via API
+ */
+const retrievedMessageToPublic = (retrievedMessage: RetrievedMessage) => ({
+  created_at: retrievedMessage.createdAt,
+  fiscal_code: retrievedMessage.fiscalCode,
+  id: retrievedMessage.id,
+  sender_service_id: retrievedMessage.senderServiceId
+});
+
+/**
+ * Handles requests for getting a single message for a recipient.
+ */
+export function GetMessageHandler(
+  messageModel: MessageModel,
+  messageStatusModel: MessageStatusModel,
+  notificationModel: NotificationModel,
+  notificationStatusModel: NotificationStatusModel,
+  blobService: BlobService
+): IGetMessageHandler {
+  return async (_, __, userAttributes, fiscalCode, messageId) => {
+    const errorOrMaybeDocument = await messageModel.findMessageForRecipient(
+      fiscalCode,
+      messageId
+    );
+
+    if (isLeft(errorOrMaybeDocument)) {
+      // the query failed
+      return ResponseErrorQuery(
+        "Error while retrieving the message",
+        errorOrMaybeDocument.value
+      );
+    }
+
+    const maybeDocument = errorOrMaybeDocument.value;
+    if (isNone(maybeDocument)) {
+      // the document does not exist
+      return ResponseErrorNotFound(
+        "Message not found",
+        "The message that you requested was not found in the system."
+      );
+    }
+
+    const retrievedMessage = maybeDocument.value;
+
+    // the service is allowed to see the message when he is the sender of the message
+    const isUserAllowed =
+      retrievedMessage.senderServiceId === userAttributes.service.serviceId;
+
+    if (!isUserAllowed) {
+      // the user is not allowed to see the message
+      return ResponseErrorForbiddenNotAuthorized;
+    }
+
+    // fetch the content of the message from the blob storage
+    const errorOrMaybeContent = await messageModel.getStoredContent(
+      blobService,
+      retrievedMessage.id,
+      retrievedMessage.fiscalCode
+    );
+
+    if (isLeft(errorOrMaybeContent)) {
+      winston.error(
+        `GetMessageHandler|${JSON.stringify(errorOrMaybeContent.value)}`
+      );
+      return ResponseErrorInternal(
+        `${errorOrMaybeContent.value.name}: ${errorOrMaybeContent.value.message}`
+      );
+    }
+
+    const content = errorOrMaybeContent.value.toUndefined();
+
+    const message = {
+      content,
+      ...retrievedMessageToPublic(retrievedMessage)
+    };
+
+    const errorOrNotificationStatuses = await getMessageNotificationStatuses(
+      notificationModel,
+      notificationStatusModel,
+      retrievedMessage.id
+    );
+
+    if (isLeft(errorOrNotificationStatuses)) {
+      return ResponseErrorInternal(
+        `Error retrieving NotificationStatus: ${errorOrNotificationStatuses.value.name}|${errorOrNotificationStatuses.value.message}`
+      );
+    }
+    const notificationStatuses = errorOrNotificationStatuses.value;
+
+    const errorOrMaybeMessageStatus = await messageStatusModel.findOneByMessageId(
+      retrievedMessage.id
+    );
+
+    if (isLeft(errorOrMaybeMessageStatus)) {
+      return ResponseErrorInternal(
+        `Error retrieving MessageStatus: ${errorOrMaybeMessageStatus.value.code}|${errorOrMaybeMessageStatus.value.body}`
+      );
+    }
+    const maybeMessageStatus = errorOrMaybeMessageStatus.value;
+
+    const returnedMessage = {
+      message,
+      notification: notificationStatuses.toUndefined(),
+      // we do not return the status date-time
+      status: maybeMessageStatus
+        .map(messageStatus => messageStatus.status)
+        // when the message has been received but a MessageStatus
+        // does not exist yet, the message is considered to be
+        // in the ACCEPTED state (not yet stored in the inbox)
+        .getOrElse(MessageStatusValueEnum.ACCEPTED)
+    };
+
+    return ResponseSuccessJson(returnedMessage);
+  };
+}
+
+/**
+ * Wraps a GetMessage handler inside an Express request handler.
+ */
+export function GetMessage(
+  serviceModel: ServiceModel,
+  messageModel: MessageModel,
+  messageStatusModel: MessageStatusModel,
+  notificationModel: NotificationModel,
+  notificationStatusModel: NotificationStatusModel,
+  blobService: BlobService
+): express.RequestHandler {
+  const handler = GetMessageHandler(
+    messageModel,
+    messageStatusModel,
+    notificationModel,
+    notificationStatusModel,
+    blobService
+  );
+  const middlewaresWrap = withRequestMiddlewares(
+    AzureApiAuthMiddleware(new Set([UserGroup.ApiMessageRead])),
+    ClientIpMiddleware,
+    AzureUserAttributesMiddleware(serviceModel),
+    FiscalCodeMiddleware,
+    RequiredParamMiddleware("id", NonEmptyString)
+  );
+  return wrapRequestHandler(
+    middlewaresWrap(
+      checkSourceIpForHandler(handler, (_, c, u, __, ___) => ipTuple(c, u))
+    )
+  );
+}
