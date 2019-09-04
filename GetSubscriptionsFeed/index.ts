4c07fcf9333ef0cada8ebc61117f9551eddda120
Federico Feroldi <pix@yahoo.it>
2019-09-04 12:02:30 +0200

    [#168133214] Client side API for subscriptions feed

---
+++ GetSubscriptionsFeed/index.ts
@@ -0,0 +1,74 @@
+import { Context } from "@azure/functions";
+import { createTableService } from "azure-storage";
+
+import * as cors from "cors";
+import * as express from "express";
+import * as winston from "winston";
+
+import { DocumentClient as DocumentDBClient } from "documentdb";
+
+import {
+  SERVICE_COLLECTION_NAME,
+  ServiceModel
+} from "io-functions-commons/dist/src/models/service";
+import * as documentDbUtils from "io-functions-commons/dist/src/utils/documentdb";
+import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
+import { secureExpressApp } from "io-functions-commons/dist/src/utils/express";
+import { AzureContextTransport } from "io-functions-commons/dist/src/utils/logging";
+import { setAppContext } from "io-functions-commons/dist/src/utils/middlewares/context_middleware";
+
+import createAzureFunctionHandler from "io-functions-express/dist/src/createAzureFunctionsHandler";
+
+import { GetSubscriptionsFeed } from "./handler";
+
+// Setup Express
+const app = express();
+secureExpressApp(app);
+
+// Set up CORS (free access to the API from browser clients)
+app.use(cors());
+
+const cosmosDbUri = getRequiredStringEnv("CUSTOMCONNSTR_COSMOSDB_URI");
+const cosmosDbKey = getRequiredStringEnv("CUSTOMCONNSTR_COSMOSDB_KEY");
+const cosmosDbName = getRequiredStringEnv("COSMOSDB_NAME");
+
+const documentDbDatabaseUrl = documentDbUtils.getDatabaseUri(cosmosDbName);
+
+const servicesCollectionUrl = documentDbUtils.getCollectionUri(
+  documentDbDatabaseUrl,
+  SERVICE_COLLECTION_NAME
+);
+
+const documentClient = new DocumentDBClient(cosmosDbUri, {
+  masterKey: cosmosDbKey
+});
+
+const serviceModel = new ServiceModel(documentClient, servicesCollectionUrl);
+
+const storageConnectionString = getRequiredStringEnv("QueueStorageConnection");
+const tableService = createTableService(storageConnectionString);
+
+const subscriptionsFeedTable = getRequiredStringEnv("SUBSCRIPTIONS_FEED_TABLE");
+
+app.get(
+  "/api/v1/subscriptions-feed/:date",
+  GetSubscriptionsFeed(serviceModel, tableService, subscriptionsFeedTable)
+);
+
+const azureFunctionHandler = createAzureFunctionHandler(app);
+
+// tslint:disable-next-line: no-let
+let logger: Context["log"] | undefined;
+const contextTransport = new AzureContextTransport(() => logger, {
+  level: "debug"
+});
+winston.add(contextTransport);
+
+// Binds the express app to an Azure Function handler
+function httpStart(context: Context): void {
+  logger = context.log;
+  setAppContext(app, context);
+  azureFunctionHandler(context);
+}
+
+export default httpStart;
---
+++ GetSubscriptionsFeed/utils.ts
@@ -0,0 +1,115 @@
+import { ServiceResponse, TableQuery, TableService } from "azure-storage";
+
+import { Either, isLeft, left, right } from "fp-ts/lib/Either";
+
+import { FiscalCodeHash } from "../generated/definitions/FiscalCodeHash";
+
+/**
+ * A minimal storage table Entry
+ */
+type TableEntry = Readonly<{
+  RowKey: Readonly<{
+    _: string;
+  }>;
+}>;
+
+/**
+ * A function that returns a page of query reults given a pagination token
+ *
+ * @see https://docs.microsoft.com/en-us/rest/api/storageservices/query-timeout-and-pagination
+ */
+export type PagedQuery = (
+  currentToken: TableService.TableContinuationToken
+) => Promise<Either<Error, TableService.QueryEntitiesResult<TableEntry>>>;
+
+/**
+ * Returns a paged query function for a certain query on a storage table
+ */
+export const getPagedQuery = (tableService: TableService, table: string) => (
+  tableQuery: TableQuery
+): PagedQuery => currentToken =>
+  new Promise(resolve =>
+    tableService.queryEntities(
+      table,
+      tableQuery,
+      currentToken,
+      (
+        error: Error,
+        result: TableService.QueryEntitiesResult<TableEntry>,
+        response: ServiceResponse
+      ) => resolve(response.isSuccessful ? right(result) : left(error))
+    )
+  );
+
+/**
+ * Iterates over all pages of entries returned by the provided paged query
+ * function.
+ *
+ * @throws Exception on query failure
+ */
+async function* iterateOnPages(
+  pagedQuery: PagedQuery
+): AsyncIterableIterator<ReadonlyArray<TableEntry>> {
+  // tslint:disable-next-line: no-let
+  let token: TableService.TableContinuationToken = null;
+  do {
+    // query for a page of entries
+    const errorOrResults = await pagedQuery(token);
+    if (isLeft(errorOrResults)) {
+      // throw an exception in case of error
+      throw errorOrResults.value;
+    }
+    // call the async callback with the current page of entries
+    const results = errorOrResults.value;
+    yield results.entries;
+    // update the continuation token, the loop will continue until
+    // the token is defined
+    token = results.continuationToken;
+  } while (token !== undefined && token !== null);
+}
+
+/**
+ * Extracts a user ID hash from a table entry.
+ *
+ * @see https://github.com/teamdigitale/io-functions-app/blob/master/UpdateSubscriptionsFeedActivity/index.ts
+ */
+const hashFromEntry = (e: TableEntry): FiscalCodeHash | undefined => {
+  const rowKey = e.RowKey._;
+  const parts = rowKey.split("-");
+  return parts.length === 6 || parts.length === 7
+    ? (parts[parts.length - 1] as FiscalCodeHash)
+    : undefined;
+};
+
+/**
+ * Do something with the user hash extracted from the table entry
+ */
+const withHashFromEntry = (f: (s: FiscalCodeHash) => void) => (
+  e: TableEntry
+): void => {
+  const hash = hashFromEntry(e);
+  if (hash !== undefined) {
+    f(hash);
+  }
+};
+
+/**
+ * Fetches all user hashed returned by the provided paged query
+ */
+export async function queryUsers(
+  pagedQuery: PagedQuery
+): Promise<ReadonlySet<FiscalCodeHash>> {
+  const entries = new Set<FiscalCodeHash>();
+  const addToSet = withHashFromEntry(s => entries.add(s));
+  for await (const page of iterateOnPages(pagedQuery)) {
+    page.forEach(addToSet);
+  }
+  return entries;
+}
+
+/**
+ * Returns a query filter to get the RowKey(s) for all entries that have the
+ * provided partition key
+ */
+export const queryFilterForKey = (partitionKey: string): TableQuery =>
+  new TableQuery().select("RowKey").where("PartitionKey == ?", partitionKey);
