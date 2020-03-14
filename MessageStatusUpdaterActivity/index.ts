e23ea885afbe6a0a493bee0f229d0d277455630e
Francesco Persico <francescopersico@gmail.com>
2020-03-14 10:32:20 +0100

    [#171800044] Use appsettings instead of connectionstrings (#34)

--- MessageStatusUpdaterActivity/index.ts
+++ MessageStatusUpdaterActivity/index.ts
@@ -10,8 +10,8 @@ import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
 
 import { getMessageStatusUpdaterActivityHandler } from "./handler";
 
-const cosmosDbUri = getRequiredStringEnv("CUSTOMCONNSTR_COSMOSDB_URI");
-const cosmosDbKey = getRequiredStringEnv("CUSTOMCONNSTR_COSMOSDB_KEY");
+const cosmosDbUri = getRequiredStringEnv("COSMOSDB_URI");
+const cosmosDbKey = getRequiredStringEnv("COSMOSDB_KEY");
 const cosmosDbName = getRequiredStringEnv("COSMOSDB_NAME");
 
 const documentDbDatabaseUrl = documentDbUtils.getDatabaseUri(cosmosDbName);
