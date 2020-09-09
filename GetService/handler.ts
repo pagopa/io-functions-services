2733bf2d425545a10a9d70a87fa4ac5972a266bf
AleDore <alessio85.dore@gmail.com>
2020-09-09 16:03:06 +0200

    [#174624167] GetService handler refactor

--- GetService/handler.ts
+++ GetService/handler.ts
@@ -21,9 +21,11 @@ import {
   wrapRequestHandler
 } from "io-functions-commons/dist/src/utils/request_middleware";
 import {
+  IResponseErrorForbiddenNotAuthorized,
   IResponseErrorInternal,
   IResponseErrorNotFound,
   IResponseSuccessJson,
+  ResponseErrorForbiddenNotAuthorized,
   ResponseErrorInternal,
   ResponseErrorNotFound,
   ResponseSuccessJson
@@ -36,20 +38,25 @@ import {
 } from "io-functions-commons/dist/src/utils/source_ip_check";
 
 import { Context } from "@azure/functions";
-import { Either, isLeft } from "fp-ts/lib/Either";
 import { identity } from "fp-ts/lib/function";
-import { tryCatch } from "fp-ts/lib/TaskEither";
+import {
+  fromLeft,
+  taskEither,
+  TaskEither,
+  tryCatch
+} from "fp-ts/lib/TaskEither";
 import { ServiceModel } from "io-functions-commons/dist/src/models/service";
 import { ContextMiddleware } from "io-functions-commons/dist/src/utils/middlewares/context_middleware";
-import { Errors } from "io-ts";
 import { IResponseType } from "italia-ts-commons/lib/requests";
+import { Service } from "../generated/api-admin/Service";
+import { SubscriptionKeys } from "../generated/api-admin/SubscriptionKeys";
 import { ServiceWithSubscriptionKeys } from "../generated/definitions/ServiceWithSubscriptionKeys";
 import { APIClient } from "../utils/clients/admin";
 import {
   IResponseErrorUnauthorized,
-  ResponseErrorUnauthorized
+  ResponseErrorUnauthorized,
+  unhandledResponseStatus
 } from "../utils/responses";
-import { FiscalCode } from "io-functions-commons/dist/generated/definitions/FiscalCode";
 
 /**
  * Type of a GetService handler.
@@ -65,32 +72,76 @@ type IGetServiceHandler = (
   attrs: IAzureUserAttributes,
   serviceId: string
 ) => Promise<
-  // tslint:disable-next-line:max-union-size
-  | IResponseSuccessJson<ServiceWithSubscriptionKeys>
-  | IResponseErrorNotFound
-  | IResponseErrorUnauthorized
-  | IResponseErrorInternal
+  IResponseSuccessJson<ServiceWithSubscriptionKeys> | ErrorResponses
 >;
 
-const toServerResponse = <S extends number, T>(
-  maybeResponse: Either<Errors, IResponseType<S, T>>
+const toErrorServerResponse = <S extends number, T>(
+  response: IResponseType<S, T>
 ) => {
-  if (isLeft(maybeResponse)) {
-    return ResponseErrorInternal(JSON.stringify(maybeResponse.value));
-  }
-  const response = maybeResponse.value;
-  if (response.status === 200) {
-    return ResponseSuccessJson(response.value);
-  }
-
   if (response.status === 401) {
     return ResponseErrorUnauthorized("Unauthorized", "Unauthorized");
+  }
+
+  if (response.status === 403) {
+    return ResponseErrorForbiddenNotAuthorized;
   }
 
   if (response.status === 404) {
     return ResponseErrorNotFound("Not found", "Resource not found");
   }
+
+  return unhandledResponseStatus(response.status);
 };
+
+type ErrorResponses =
+  | IResponseErrorNotFound
+  | IResponseErrorUnauthorized
+  | IResponseErrorForbiddenNotAuthorized
+  | IResponseErrorInternal;
+
+const getServiceTask = (
+  apiClient: ReturnType<APIClient>,
+  serviceId: string
+): TaskEither<ErrorResponses, Service> =>
+  tryCatch(
+    () =>
+      apiClient.getService({
+        service_id: serviceId
+      }),
+    errs => ResponseErrorInternal(JSON.stringify(errs))
+  ).foldTaskEither(
+    err => fromLeft(err),
+    maybeResponse =>
+      maybeResponse.fold(
+        errs => fromLeft(ResponseErrorInternal(JSON.stringify(errs))),
+        responseType =>
+          responseType.status !== 200
+            ? fromLeft(toErrorServerResponse(responseType))
+            : taskEither.of(responseType.value)
+      )
+  );
+
+const getSubscriptionKeysTask = (
+  apiClient: ReturnType<APIClient>,
+  serviceId: string
+): TaskEither<ErrorResponses, SubscriptionKeys> =>
+  tryCatch(
+    () =>
+      apiClient.getSubscriptionKeys({
+        service_id: serviceId
+      }),
+    errs => ResponseErrorInternal(JSON.stringify(errs))
+  ).foldTaskEither(
+    err => fromLeft(err),
+    maybeResponse =>
+      maybeResponse.fold(
+        errs => fromLeft(ResponseErrorInternal(JSON.stringify(errs))),
+        responseType =>
+          responseType.status !== 200
+            ? fromLeft(toErrorServerResponse(responseType))
+            : taskEither.of(responseType.value)
+      )
+  );
 
 /**
  * Handles requests for getting a single service by a service ID.
@@ -98,19 +149,21 @@ const toServerResponse = <S extends number, T>(
 export function GetServiceHandler(
   apiClient: ReturnType<APIClient>
 ): IGetServiceHandler {
-  return (context, _, __, userAttributes, serviceId) => {
-    const x = tryCatch(
-      () =>
-        apiClient.getService({
-          service_id: serviceId
-        }),
-      errs => ResponseErrorInternal(JSON.stringify(errs))
-    )
-      .map(res => toServerResponse(res))
-      .fold(identity, identity)
+  return (_, __, ___, ____, serviceId) => {
+    return getServiceTask(apiClient, serviceId)
+      .chain(service =>
+        getSubscriptionKeysTask(apiClient, serviceId).map(subscriptionKeys =>
+          ResponseSuccessJson({
+            ...service,
+            ...subscriptionKeys
+          })
+        )
+      )
+      .fold<IResponseSuccessJson<ServiceWithSubscriptionKeys> | ErrorResponses>(
+        identity,
+        identity
+      )
       .run();
-
-    return x;
   };
 }
 
