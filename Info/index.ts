587b2490fa75252632fe0ca886749d72d22dc82b
Emanuele De Cupis <ema.decu@gmail.com>
2020-11-26 16:07:02 +0100

    fix info path

--- Info/index.ts
+++ Info/index.ts
@@ -10,7 +10,7 @@ const app = express();
 secureExpressApp(app);
 
 // Add express route
-app.get("/api/v1/info", Info());
+app.get("/info", Info());
 
 const azureFunctionHandler = createAzureFunctionHandler(app);
 
