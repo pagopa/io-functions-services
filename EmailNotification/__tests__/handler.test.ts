4eb3a39720a46741621a12d9c4fea61e9ba9ee6b
Emanuele De Cupis <ema.decu@gmail.com>
2021-11-13 00:49:12 +0100

    [#ic-87] EmailNotification/1 copy from EmailNotificationActivity

---
+++ EmailNotification/__tests__/handler.test.ts
@@ -0,0 +1,170 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable sonarjs/no-duplicate-string */
+/* eslint-disable sonar/sonar-max-lines-per-function */
+/* eslint-disable sonarjs/no-identical-functions */
+
+import {
+  NonEmptyString,
+  OrganizationFiscalCode
+} from "@pagopa/ts-commons/lib/strings";
+
+import {
+  EmailNotificationInput,
+  getEmailNotificationHandler
+} from "../handler";
+
+import { pipe } from "fp-ts/lib/function";
+import * as E from "fp-ts/lib/Either";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as O from "fp-ts/lib/Option";
+
+import { EmailAddress } from "@pagopa/io-functions-commons/dist/generated/definitions/EmailAddress";
+import { MessageBodyMarkdown } from "@pagopa/io-functions-commons/dist/generated/definitions/MessageBodyMarkdown";
+import { MessageContent } from "@pagopa/io-functions-commons/dist/generated/definitions/MessageContent";
+import { MessageSubject } from "@pagopa/io-functions-commons/dist/generated/definitions/MessageSubject";
+import { TimeToLiveSeconds } from "@pagopa/io-functions-commons/dist/generated/definitions/TimeToLiveSeconds";
+
+import { NotificationChannelEnum } from "@pagopa/io-functions-commons/dist/generated/definitions/NotificationChannel";
+import * as mail from "@pagopa/io-functions-commons/dist/src/mailer/transports";
+import { CreatedMessageEventSenderMetadata } from "@pagopa/io-functions-commons/dist/src/models/created_message_sender_metadata";
+import {
+  NewNotification,
+  NotificationAddressSourceEnum,
+  NotificationModel,
+  RetrievedNotification
+} from "@pagopa/io-functions-commons/dist/src/models/notification";
+
+beforeEach(() => jest.clearAllMocks());
+
+const mockContext = {
+  executionContext: { functionName: "funcname" },
+  log: {
+    // eslint-disable-next-line no-console
+    error: console.error,
+    // eslint-disable-next-line no-console
+    info: console.log,
+    // eslint-disable-next-line no-console
+    verbose: console.log,
+    // eslint-disable-next-line no-console
+    warn: console.warn
+  }
+} as any;
+
+const aMessageId = "A_MESSAGE_ID" as NonEmptyString;
+
+const aNewEmailNotification: NewNotification = {
+  channels: {
+    [NotificationChannelEnum.EMAIL]: {
+      addressSource: NotificationAddressSourceEnum.DEFAULT_ADDRESS,
+      toAddress: "to@example.com" as EmailAddress
+    }
+  },
+  fiscalCode: "FRLFRC74E04B157I" as any,
+  id: "A_NOTIFICATION_ID" as NonEmptyString,
+  kind: "INewNotification",
+  messageId: aMessageId
+};
+
+const aRetrievedNotification: RetrievedNotification = {
+  _etag: "_etag",
+  _rid: "_rid",
+  _self: "_self",
+  _ts: 1,
+  ...aNewEmailNotification,
+  kind: "IRetrievedNotification"
+};
+
+const notificationModelMock = ({
+  find: jest.fn(() => TE.of(O.some(aRetrievedNotification)))
+} as unknown) as NotificationModel;
+
+const aNotificationId = "A_NOTIFICATION_ID" as NonEmptyString;
+const anOrganizationFiscalCode = "00000000000" as OrganizationFiscalCode;
+
+const aMessageBodyMarkdown = "test".repeat(80) as MessageBodyMarkdown;
+
+const aMessageContent: MessageContent = {
+  markdown: aMessageBodyMarkdown,
+  subject: "test".repeat(10) as MessageSubject
+};
+
+const aMessage = {
+  createdAt: new Date(),
+  fiscalCode: "FRLFRC74E04B157I" as any,
+  id: aMessageId,
+  indexedId: aMessageId,
+  kind: "INewMessageWithoutContent" as "INewMessageWithoutContent",
+  senderServiceId: "s123" as NonEmptyString,
+  senderUserId: "u123" as NonEmptyString,
+  timeToLiveSeconds: 3600 as TimeToLiveSeconds
+};
+
+const aSenderMetadata: CreatedMessageEventSenderMetadata = {
+  departmentName: "IT" as NonEmptyString,
+  organizationFiscalCode: anOrganizationFiscalCode,
+  organizationName: "AgID" as NonEmptyString,
+  requireSecureChannels: false,
+  serviceName: "Test" as NonEmptyString,
+  serviceUserEmail: "email@example.com" as EmailAddress
+};
+
+const HTML_TO_TEXT_OPTIONS: HtmlToTextOptions = {
+  ignoreImage: true, // ignore all document images
+  tables: true
+};
+
+const MAIL_FROM = "IO - l’app dei servizi pubblici <no-reply@io.italia.it>" as NonEmptyString;
+const defaultNotificationParams = {
+  HTML_TO_TEXT_OPTIONS,
+  MAIL_FROM
+};
+
+const input: EmailNotificationInput = {
+  notificationEvent: {
+    content: aMessageContent,
+    message: aMessage,
+    notificationId: aNotificationId,
+    senderMetadata: aSenderMetadata
+  }
+};
+
+const lMailerTransporterMock = ({} as unknown) as mail.MailerTransporter;
+
+describe("getEmailNotificationActivityHandler", () => {
+  it("should respond with 'SUCCESS' if the mail is sent", async () => {
+    jest.spyOn(mail, "sendMail").mockReturnValueOnce(TE.of("SUCCESS"));
+
+    const GetEmailNotificationActivityHandler = getEmailNotificationHandler(
+      lMailerTransporterMock,
+      notificationModelMock,
+      defaultNotificationParams
+    );
+
+    const result = await GetEmailNotificationActivityHandler(
+      mockContext,
+      input
+    );
+
+    expect(result.kind).toBe("SUCCESS");
+  });
+
+  it("should respond with 'ERROR' if the mail is not sent", async () => {
+    const errorMessage: string = "Test Error";
+
+    jest
+      .spyOn(mail, "sendMail")
+      .mockReturnValueOnce(TE.left(new Error(errorMessage)));
+
+    const GetEmailNotificationActivityHandler = getEmailNotificationHandler(
+      lMailerTransporterMock,
+      notificationModelMock,
+      defaultNotificationParams
+    );
+
+    try {
+      await GetEmailNotificationActivityHandler(mockContext, input);
+    } catch (e) {
+      expect(e.message).toBe("Error while sending email: " + errorMessage);
+    }
+  });
+});
---
+++ EmailNotification/index.ts
@@ -0,0 +1,52 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ *
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+import { AzureFunction } from "@azure/functions";
+
+import {
+  NOTIFICATION_COLLECTION_NAME,
+  NotificationModel
+} from "@pagopa/io-functions-commons/dist/src/models/notification";
+
+import { getMailerTransporter } from "@pagopa/io-functions-commons/dist/src/mailer";
+import { cosmosdbInstance } from "../utils/cosmosdb";
+import { getConfigOrThrow } from "../utils/config";
+import { getEmailNotificationHandler } from "./handler";
+
+const config = getConfigOrThrow();
+
+const notificationModel = new NotificationModel(
+  cosmosdbInstance.container(NOTIFICATION_COLLECTION_NAME)
+);
+
+//
+// options used when converting an HTML message to pure text
+// see https://www.npmjs.com/package/html-to-text#options
+//
+const HTML_TO_TEXT_OPTIONS: HtmlToTextOptions = {
+  ignoreImage: true, // ignore all document images
+  tables: true
+};
+
+// default sender for email
+const MAIL_FROM = config.MAIL_FROM;
+
+const mailerTransporter = getMailerTransporter(config);
+
+const activityFunction: AzureFunction = getEmailNotificationHandler(
+  mailerTransporter,
+  notificationModel,
+  {
+    HTML_TO_TEXT_OPTIONS,
+    MAIL_FROM
+  }
+);
+
+export default activityFunction;
