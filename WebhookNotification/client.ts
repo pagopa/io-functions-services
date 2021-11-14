e343ba941496cc5704367cdde6ac20e4011e2b08
Emanuele De Cupis <ema.decu@gmail.com>
2021-11-14 17:56:03 +0100

    [#ic-87] WebhookNotification/1 copy from WebhookNotificationActrivity

---
+++ WebhookNotification/client.ts
@@ -0,0 +1,36 @@
+import { HttpsUrl } from "@pagopa/io-functions-commons/dist/generated/definitions/HttpsUrl";
+import {
+  ApiHeaderJson,
+  createFetchRequestForApi,
+  TypeofApiCall
+} from "@pagopa/ts-commons/lib/requests";
+import * as r from "@pagopa/ts-commons/lib/requests";
+import { ProblemJson } from "@pagopa/ts-commons/lib/responses";
+import { Notification } from "../generated/notifications/Notification";
+import { notifyDefaultDecoder } from "../generated/notifications/requestTypes";
+import { SuccessResponse } from "../generated/notifications/SuccessResponse";
+
+export type WebhookNotifyT = r.IPostApiRequestType<
+  { readonly notification: Notification; readonly webhookEndpoint: HttpsUrl },
+  "Content-Type",
+  never,
+  | r.IResponseType<200, SuccessResponse>
+  | r.IResponseType<400, ProblemJson>
+  | r.IResponseType<401, undefined>
+  | r.IResponseType<500, ProblemJson>
+>;
+
+export const getNotifyClient = (
+  fetchApi: typeof fetch
+): TypeofApiCall<WebhookNotifyT> =>
+  createFetchRequestForApi(
+    {
+      body: params => JSON.stringify(params.notification),
+      headers: ApiHeaderJson,
+      method: "post",
+      query: _ => ({}),
+      response_decoder: notifyDefaultDecoder(),
+      url: params => `${params.webhookEndpoint}`
+    } as WebhookNotifyT,
+    { fetchApi }
+  );
