8e24e2ad97fe52ed0a663b11b9b46f66b606b96a
gquadrati <greta.quadrati@pagopa.it>
2021-11-17 11:56:43 +0100

    [#ic-87] add queues and blobs

--- docker/fixtures/src/azure_storage.ts
+++ docker/fixtures/src/azure_storage.ts
@@ -1,18 +1,35 @@
 import { BlobServiceClient } from "@azure/storage-blob";
+import { QueueServiceClient } from "@azure/storage-queue";
 import { log } from "./utils/logger";
 
 export const fillAzureStorage = async (
   QueueStorageConnection: string
 ): Promise<void> => {
-  log("Creating Queue Storages");
+  const blobs = process.env.BLOBS.split(",");
+
+  log(`Creating ${blobs.length} Queue Storages`);
   const blobServiceClient = BlobServiceClient.fromConnectionString(
     QueueStorageConnection
   );
 
-  await Promise.all([
-    blobServiceClient.createContainer("message-content"),
-    blobServiceClient.createContainer("push-notifications")
-  ]);
+  const resB = await Promise.all(
+    blobs.map(b => blobServiceClient.createContainer(b))
+  );
 
-  log("Queue Storages created");
+  log(`${resB.length} Blob Storages created`);
+
+  // -------
+
+  const queues = process.env.QUEUES.split(",");
+
+  log(`Creating ${queues.length} Queue Storages`);
+  const queuServiceClient = QueueServiceClient.fromConnectionString(
+    QueueStorageConnection
+  );
+
+  const resQ = await Promise.all(
+    queues.map(q => queuServiceClient.createQueue(q))
+  );
+
+  log(`${resQ.length} Queue Storages created`);
 };
