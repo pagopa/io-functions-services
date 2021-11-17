f4cdb3f1db618c31a3b83a4c7307a5b03f4db73e
gquadrati <greta.quadrati@pagopa.it>
2021-11-17 14:44:17 +0100

    [#ic-87] fix fixtures

--- docker/fixtures/src/azure_storage.ts
+++ docker/fixtures/src/azure_storage.ts
@@ -1,35 +1,78 @@
 import { BlobServiceClient } from "@azure/storage-blob";
 import { QueueServiceClient } from "@azure/storage-queue";
+import { flow, pipe } from "fp-ts/lib/function";
+import * as RA from "fp-ts/lib/ReadonlyArray";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as E from "fp-ts/lib/Either";
+import * as T from "fp-ts/lib/Task";
 import { log } from "./utils/logger";
+
+const createQueues = (
+  queueServiceClient: QueueServiceClient,
+  queues: string[]
+) =>
+  pipe(
+    queues,
+    T.of,
+    T.chain(
+      flow(
+        RA.map(q =>
+          TE.tryCatch(async () => queueServiceClient.createQueue(q), E.toError)
+        ),
+        RA.sequence(T.ApplicativeSeq)
+      )
+    )
+  );
+
+const createBlobs = (blobServiceClient: BlobServiceClient, blobs: string[]) =>
+  pipe(
+    blobs,
+    T.of,
+    T.chain(
+      flow(
+        RA.map(b =>
+          TE.tryCatch(
+            async () => blobServiceClient.createContainer(b),
+            E.toError
+          )
+        ),
+        RA.sequence(T.ApplicativeSeq)
+      )
+    )
+  );
 
 export const fillAzureStorage = async (
   QueueStorageConnection: string
 ): Promise<void> => {
   const blobs = process.env.BLOBS.split(",");
 
-  log(`Creating ${blobs.length} Queue Storages`);
+  log(`Creating ${blobs.length} Blob Storages`);
   const blobServiceClient = BlobServiceClient.fromConnectionString(
     QueueStorageConnection
   );
 
-  const resB = await Promise.all(
-    blobs.map(b => blobServiceClient.createContainer(b))
-  );
+  const resB = await createBlobs(blobServiceClient, blobs)();
 
-  log(`${resB.length} Blob Storages created`);
+  const [rightsB, leftsB] = [RA.rights(resB), RA.lefts(resB)];
+
+  log(`${rightsB.length} Blob Storages created`);
+  log(`${leftsB.length} Blob Storages not created`);
+  log(leftsB);
 
   // -------
 
   const queues = process.env.QUEUES.split(",");
 
   log(`Creating ${queues.length} Queue Storages`);
-  const queuServiceClient = QueueServiceClient.fromConnectionString(
+  const queueServiceClient = QueueServiceClient.fromConnectionString(
     QueueStorageConnection
   );
 
-  const resQ = await Promise.all(
-    queues.map(q => queuServiceClient.createQueue(q))
-  );
+  const resQ = await createQueues(queueServiceClient, queues)();
 
-  log(`${resQ.length} Queue Storages created`);
+  const [rightsQ, leftsQ] = [RA.rights(resQ), RA.lefts(resQ)];
+
+  log(`${rightsQ.length} Queue Storages created`);
+  log(`${leftsQ.length} Queue Storages NOT created`);
+  log(leftsQ);
 };
