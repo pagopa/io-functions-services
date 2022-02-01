241531dfba91971b56ab931f263a4c784d2245b1
AleDore <alessio85.dore@gmail.com>
2022-02-01 17:11:16 +0100

    Add MVL multiprovider Sender service

--- utils/mvl-service-mapper.ts
+++ utils/mvl-service-mapper.ts
@@ -4,10 +4,19 @@ export const FIXED_PEC_MAP: Record<string, ILegalMessageMap> = {
   "certificati-anpr@pec.io.italia.poste.it": {
     serviceId: "01FBHRG29AHHRP42M3EHGZW55H"
   },
+  "certificati-anpr@test.pec.io.italia.aruba.it": {
+    serviceId: "01FBHRG29AHHRP42M3EHGZW55H"
+  },
   "piattaforma-notifiche@pec.io.italia.poste.it": {
     serviceId: "01FBHR67HKNQG9JP3GPJEMGXV2"
   },
+  "piattaforma-notifiche@test.pec.io.italia.aruba.it": {
+    serviceId: "01FBHR67HKNQG9JP3GPJEMGXV2"
+  },
   "protocollo-ipazia@pec.io.italia.poste.it": {
+    serviceId: "01FM05GA5QXECVHV7D6E7EY8FW"
+  },
+  "protocollo-ipazia@test.pec.io.italia.aruba.it": {
     serviceId: "01FM05GA5QXECVHV7D6E7EY8FW"
   },
   "test@legal.it": { serviceId: "aValidServiceId" }
