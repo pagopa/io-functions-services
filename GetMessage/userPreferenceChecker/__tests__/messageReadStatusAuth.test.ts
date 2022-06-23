1d260fa1caee26d215781123c1f6741c5292119b
gquadrati <greta.quadrati@pagopa.it>
2022-06-24 00:35:08 +0200

    [#ICC-101] add unit tests

---
+++ GetMessage/userPreferenceChecker/__tests__/messageReadStatusAuth.test.ts
@@ -0,0 +1,261 @@
+import * as TE from "fp-ts/TaskEither";
+import * as E from "fp-ts/Either";
+import * as O from "fp-ts/Option";
+
+import { Semver } from "@pagopa/ts-commons/lib/strings";
+
+import { CosmosErrors } from "@pagopa/io-functions-commons/dist/src/utils/cosmosdb_model";
+import { ProfileModel } from "@pagopa/io-functions-commons/dist/src/models/profile";
+import {
+  AccessReadMessageStatusEnum,
+  RetrievedServicePreference,
+  ServicesPreferencesModel
+} from "@pagopa/io-functions-commons/dist/src/models/service_preference";
+
+import { canAccessMessageReadStatus } from "../messageReadStatusAuth";
+
+import {
+  aFiscalCode,
+  aRetrievedProfile,
+  aServiceId,
+  autoProfileServicePreferencesSettings,
+  legacyProfileServicePreferencesSettings
+} from "../../../__mocks__/mocks";
+
+const MIN_READ_STATUS_PREFERENCES_VERSION = "1.5.3" as Semver;
+const PREV_APP_VERIONS = "1.3.8" as Semver;
+const NEWER_APP_VERSION = "3.1.1" as Semver;
+
+const aRetrievedServicePreference: RetrievedServicePreference = {
+  fiscalCode: aFiscalCode,
+  serviceId: aServiceId,
+  isInboxEnabled: true,
+  isEmailEnabled: true,
+  isWebhookEnabled: true
+} as RetrievedServicePreference;
+
+// --------------------
+
+const mockProfileFindLastVersionByModelId = jest.fn();
+const mockServicePreferencesFind = jest.fn();
+
+const profileModel = ({
+  //TaskEither<CosmosErrors, O.Option<TR>>
+  findLastVersionByModelId: mockProfileFindLastVersionByModelId
+} as unknown) as ProfileModel;
+
+const servicePreferenceModel = ({
+  find: mockServicePreferencesFind
+} as unknown) as ServicesPreferencesModel;
+
+// ----------------------------
+// Tests
+// ----------------------------
+
+describe("canAccessMessageReadStatus |> ok", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return false if profile version is UNKNOWN", async () => {
+    mockProfileFindLastVersionByModelId.mockReturnValueOnce(
+      TE.of(O.some(aRetrievedProfile))
+    );
+
+    const res = await canAccessMessageReadStatus(
+      profileModel,
+      servicePreferenceModel,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    )(aServiceId, aFiscalCode)();
+
+    expect(res).toStrictEqual(E.right(false));
+
+    // Do not call service preferences if app version is UNKNOWN
+    expect(mockServicePreferencesFind).not.toHaveBeenCalled();
+  });
+
+  it("should return false if profile version is < MIN_READ_STATUS_PREFERENCES_VERSION", async () => {
+    mockProfileFindLastVersionByModelId.mockReturnValueOnce(
+      TE.of(O.some({ ...aRetrievedProfile, lastAppVersion: PREV_APP_VERIONS }))
+    );
+
+    const res = await canAccessMessageReadStatus(
+      profileModel,
+      servicePreferenceModel,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    )(aServiceId, aFiscalCode)();
+
+    expect(res).toStrictEqual(E.right(false));
+
+    // Do not call service preferences if app version is UNKNOWN
+    expect(mockServicePreferencesFind).not.toHaveBeenCalled();
+  });
+
+  it("should return true if profile version is = MIN_READ_STATUS_PREFERENCES_VERSION", async () => {
+    mockProfileFindLastVersionByModelId.mockReturnValueOnce(
+      TE.of(
+        O.some({
+          ...aRetrievedProfile,
+          lastAppVersion: MIN_READ_STATUS_PREFERENCES_VERSION,
+          servicePreferencesSettings: autoProfileServicePreferencesSettings
+        })
+      )
+    );
+
+    mockServicePreferencesFind.mockReturnValueOnce(TE.of(O.none));
+
+    const res = await canAccessMessageReadStatus(
+      profileModel,
+      servicePreferenceModel,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    )(aServiceId, aFiscalCode)();
+
+    expect(res).toStrictEqual(E.right(true));
+
+    expect(mockServicePreferencesFind).toHaveBeenCalled();
+  });
+
+  it("should return true if profile version is > MIN_READ_STATUS_PREFERENCES_VERSION", async () => {
+    mockProfileFindLastVersionByModelId.mockReturnValueOnce(
+      TE.of(
+        O.some({
+          ...aRetrievedProfile,
+          lastAppVersion: NEWER_APP_VERSION,
+          servicePreferencesSettings: autoProfileServicePreferencesSettings
+        })
+      )
+    );
+
+    mockServicePreferencesFind.mockReturnValueOnce(TE.of(O.none));
+
+    const res = await canAccessMessageReadStatus(
+      profileModel,
+      servicePreferenceModel,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    )(aServiceId, aFiscalCode)();
+
+    expect(res).toStrictEqual(E.right(true));
+
+    expect(mockServicePreferencesFind).toHaveBeenCalled();
+  });
+
+  it("should return false if profile version is > MIN_READ_STATUS_PREFERENCES_VERSION and service settings is set to DENY", async () => {
+    mockProfileFindLastVersionByModelId.mockReturnValueOnce(
+      TE.of(
+        O.some({
+          ...aRetrievedProfile,
+          lastAppVersion: NEWER_APP_VERSION,
+          servicePreferencesSettings: autoProfileServicePreferencesSettings
+        })
+      )
+    );
+
+    mockServicePreferencesFind.mockReturnValueOnce(
+      TE.of(
+        O.some({
+          ...aRetrievedServicePreference,
+          accessReadMessageStatus: AccessReadMessageStatusEnum.DENY
+        })
+      )
+    );
+
+    const res = await canAccessMessageReadStatus(
+      profileModel,
+      servicePreferenceModel,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    )(aServiceId, aFiscalCode)();
+
+    expect(res).toStrictEqual(E.right(false));
+
+    expect(mockServicePreferencesFind).toHaveBeenCalled();
+  });
+});
+
+describe("canAccessMessageReadStatus |> Errors", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return an Error if profileModel returns an error", async () => {
+    mockProfileFindLastVersionByModelId.mockReturnValueOnce(
+      TE.left({} as CosmosErrors)
+    );
+
+    const res = await canAccessMessageReadStatus(
+      profileModel,
+      servicePreferenceModel,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    )(aServiceId, aFiscalCode)();
+
+    expect(res).toStrictEqual(
+      E.left(Error("Error retrieving user profile from Cosmos DB"))
+    );
+
+    // Do not call service preferences if app version is UNKNOWN
+    expect(mockServicePreferencesFind).not.toHaveBeenCalled();
+  });
+
+  it("should return an Error if profileModel returns O.none", async () => {
+    mockProfileFindLastVersionByModelId.mockReturnValueOnce(TE.right(O.none));
+
+    const res = await canAccessMessageReadStatus(
+      profileModel,
+      servicePreferenceModel,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    )(aServiceId, aFiscalCode)();
+
+    expect(res).toStrictEqual(E.left(Error("Profile not found")));
+
+    // Do not call service preferences if app version is UNKNOWN
+    expect(mockServicePreferencesFind).not.toHaveBeenCalled();
+  });
+
+  it("should return an Error if servicePreferenceModel returns an Error", async () => {
+    mockProfileFindLastVersionByModelId.mockReturnValueOnce(
+      TE.of(
+        O.some({
+          ...aRetrievedProfile,
+          lastAppVersion: MIN_READ_STATUS_PREFERENCES_VERSION,
+          servicePreferencesSettings: autoProfileServicePreferencesSettings
+        })
+      )
+    );
+
+    mockServicePreferencesFind.mockReturnValueOnce(TE.left({} as CosmosErrors));
+
+    const res = await canAccessMessageReadStatus(
+      profileModel,
+      servicePreferenceModel,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    )(aServiceId, aFiscalCode)();
+
+    expect(res).toStrictEqual(
+      E.left(Error("Error retrieving user' service preferences from Cosmos DB"))
+    );
+  });
+
+  it("should return an Error if profile servicePreferencesSettings is of type LEGACY", async () => {
+    mockProfileFindLastVersionByModelId.mockReturnValueOnce(
+      TE.of(
+        O.some({
+          ...aRetrievedProfile,
+          lastAppVersion: MIN_READ_STATUS_PREFERENCES_VERSION,
+          servicePreferencesSettings: legacyProfileServicePreferencesSettings
+        })
+      )
+    );
+
+    const res = await canAccessMessageReadStatus(
+      profileModel,
+      servicePreferenceModel,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    )(aServiceId, aFiscalCode)();
+
+    expect(res).toStrictEqual(
+      E.left(Error("Legacy service preferences not allowed"))
+    );
+
+    // Do not call service preferences if app version is UNKNOWN
+    expect(mockServicePreferencesFind).not.toHaveBeenCalled();
+  });
+});
---
+++ GetMessage/userPreferenceChecker/__tests__/userPreferencesCheckerFactory.test.ts
@@ -0,0 +1,220 @@
+import * as TE from "fp-ts/TaskEither";
+import * as E from "fp-ts/Either";
+import * as O from "fp-ts/Option";
+
+import { Semver } from "@pagopa/ts-commons/lib/strings";
+
+import {
+  AccessReadMessageStatusEnum,
+  ServicePreference
+} from "@pagopa/io-functions-commons/dist/src/models/service_preference";
+
+import {
+  appVersionHandleReadAuth,
+  ServicePreferencesGetter,
+  userPreferenceCheckerVersionUNKNOWNToVersionWithReadAuth,
+  userPreferenceCheckerVersionWithReadAuth,
+  userPreferencesCheckerFactory
+} from "../userPreferencesCheckerFactory";
+
+import {
+  aFiscalCode,
+  aRetrievedProfile,
+  aServiceId
+} from "../../../__mocks__/mocks";
+
+const MIN_READ_STATUS_PREFERENCES_VERSION = "1.5.3" as Semver;
+const PREV_APP_VERIONS = "1.3.8" as Semver;
+const NEWER_APP_VERSION = "3.1.1" as Semver;
+
+const aServicePreferenceWithUNKNOWN: ServicePreference = {
+  fiscalCode: aFiscalCode,
+  serviceId: aServiceId,
+  isInboxEnabled: true,
+  isEmailEnabled: true,
+  isWebhookEnabled: true,
+  accessReadMessageStatus: AccessReadMessageStatusEnum.UNKNOWN
+} as ServicePreference;
+
+const aServicePreferenceWithALLOW = {
+  ...aServicePreferenceWithUNKNOWN,
+  accessReadMessageStatus: AccessReadMessageStatusEnum.ALLOW
+};
+
+const aServicePreferenceWithDENY = {
+  ...aServicePreferenceWithUNKNOWN,
+  accessReadMessageStatus: AccessReadMessageStatusEnum.DENY
+};
+
+const mockServicePreferencesGetter = jest.fn(((_fiscalCode, _serviceId) =>
+  TE.of(O.none)) as ServicePreferencesGetter);
+
+describe("userPreferencesCheckerFactory |> userPreferenceCheckerVersionWithReadAuth", () => {
+  it("should return an Error if servicePreferencesGetter returns an Error", async () => {
+    mockServicePreferencesGetter.mockReturnValueOnce(
+      TE.left(Error("an Error"))
+    );
+
+    const res = await userPreferenceCheckerVersionWithReadAuth(
+      mockServicePreferencesGetter
+    ).canAccessMessageReadStatus(aServiceId, aFiscalCode)();
+
+    expect(res).toStrictEqual(E.left(Error("an Error")));
+  });
+
+  it.each`
+    title                                                                                              | I_servicePreference                      | O_expectedResult
+    ${"should return true if service preference does not exist"}                                       | ${O.none}                                | ${true}
+    ${"should return true if service preference exists and accessReadMessageStatus is set to ALLOW"}   | ${O.some(aServicePreferenceWithALLOW)}   | ${true}
+    ${"should return true if service preference exists and accessReadMessageStatus is set to UNKNOWN"} | ${O.some(aServicePreferenceWithUNKNOWN)} | ${true}
+    ${"should return false if service preference exists and accessReadMessageStatus is set to DENY"}   | ${O.some(aServicePreferenceWithDENY)}    | ${false}
+  `("$title", async ({ I_servicePreference, O_expectedResult }) => {
+    mockServicePreferencesGetter.mockReturnValueOnce(
+      TE.of(I_servicePreference)
+    );
+
+    const res = await userPreferenceCheckerVersionWithReadAuth(
+      mockServicePreferencesGetter
+    ).canAccessMessageReadStatus(aServiceId, aFiscalCode)();
+
+    expect(res).toStrictEqual(E.right(O_expectedResult));
+  });
+});
+
+describe("userPreferencesCheckerFactory |> userPreferenceCheckerVersionUNKNOWNToVersionWithReadAuth", () => {
+  it.each`
+    title                                                                                               | I_servicePreference                      | O_expectedResult
+    ${"should return false if service preference does not exist"}                                       | ${O.none}                                | ${false}
+    ${"should return false if service preference exists and accessReadMessageStatus is set to ALLOW"}   | ${O.some(aServicePreferenceWithALLOW)}   | ${false}
+    ${"should return false if service preference exists and accessReadMessageStatus is set to UNKNOWN"} | ${O.some(aServicePreferenceWithUNKNOWN)} | ${false}
+    ${"should return false if service preference exists and accessReadMessageStatus is set to DENY"}    | ${O.some(aServicePreferenceWithDENY)}    | ${false}
+  `("$title", async ({ I_servicePreference, O_expectedResult }) => {
+    mockServicePreferencesGetter.mockReturnValueOnce(
+      TE.of(I_servicePreference)
+    );
+
+    const res = await userPreferenceCheckerVersionUNKNOWNToVersionWithReadAuth.canAccessMessageReadStatus(
+      aServiceId,
+      aFiscalCode
+    )();
+
+    expect(res).toStrictEqual(E.right(O_expectedResult));
+  });
+});
+
+describe("userPreferencesCheckerFactory |> userPreferencesCheckerFactory", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const f = require("../userPreferencesCheckerFactory");
+
+  // userPreferenceCheckerVersionUNKNOWNToVersionWithReadAuth,
+  //   userPreferenceCheckerVersionWithReadAuth,
+  const spyUnknownImplementation = jest.spyOn(
+    userPreferenceCheckerVersionUNKNOWNToVersionWithReadAuth,
+    "canAccessMessageReadStatus"
+  );
+  const spyVersionGreaterThanImplementation = jest.spyOn(
+    f,
+    "userPreferenceCheckerVersionWithReadAuth"
+  );
+
+  it("should call userPreferenceCheckerVersionWithReadAuth if appVersion is UNKNOWN", async () => {
+    mockServicePreferencesGetter.mockReturnValueOnce(TE.of(O.none));
+
+    const res = await userPreferencesCheckerFactory(
+      {
+        ...aRetrievedProfile,
+        lastAppVersion: "UNKNOWN"
+      },
+      mockServicePreferencesGetter,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    ).canAccessMessageReadStatus(aServiceId, aFiscalCode)();
+
+    expect(spyUnknownImplementation).toHaveBeenCalled();
+    expect(spyVersionGreaterThanImplementation).not.toHaveBeenCalled();
+
+    expect(res).toStrictEqual(E.right(false));
+  });
+
+  it("should call userPreferenceCheckerVersionWithReadAuth if appVersion is < MIN_READ_STATUS_PREFERENCES_VERSION", async () => {
+    mockServicePreferencesGetter.mockReturnValueOnce(TE.of(O.none));
+
+    const res = await userPreferencesCheckerFactory(
+      {
+        ...aRetrievedProfile,
+        lastAppVersion: PREV_APP_VERIONS
+      },
+      mockServicePreferencesGetter,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    ).canAccessMessageReadStatus(aServiceId, aFiscalCode)();
+
+    expect(spyUnknownImplementation).toHaveBeenCalled();
+    expect(spyVersionGreaterThanImplementation).not.toHaveBeenCalled();
+
+    expect(res).toStrictEqual(E.right(false));
+  });
+
+  it("should call userPreferenceCheckerVersionUNKNOWNToVersionWithReadAuth if appVersion is = MIN_READ_STATUS_PREFERENCES_VERSION", async () => {
+    mockServicePreferencesGetter.mockReturnValueOnce(TE.of(O.none));
+
+    const res = await userPreferencesCheckerFactory(
+      {
+        ...aRetrievedProfile,
+        lastAppVersion: MIN_READ_STATUS_PREFERENCES_VERSION
+      },
+      mockServicePreferencesGetter,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    ).canAccessMessageReadStatus(aServiceId, aFiscalCode)();
+
+    expect(spyUnknownImplementation).not.toHaveBeenCalled();
+    expect(spyVersionGreaterThanImplementation).toHaveBeenCalled();
+
+    expect(res).toStrictEqual(E.right(true));
+  });
+
+  it("should call userPreferenceCheckerVersionUNKNOWNToVersionWithReadAuth if appVersion is > MIN_READ_STATUS_PREFERENCES_VERSION", async () => {
+    mockServicePreferencesGetter.mockReturnValueOnce(TE.of(O.none));
+
+    const res = await userPreferencesCheckerFactory(
+      {
+        ...aRetrievedProfile,
+        lastAppVersion: NEWER_APP_VERSION
+      },
+      mockServicePreferencesGetter,
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    ).canAccessMessageReadStatus(aServiceId, aFiscalCode)();
+
+    expect(spyUnknownImplementation).not.toHaveBeenCalled();
+    expect(spyVersionGreaterThanImplementation).toHaveBeenCalled();
+
+    expect(res).toStrictEqual(E.right(true));
+  });
+});
+
+describe("appVersionHandleReadAuth", () => {
+  it("should return false if currentAppVersion is lower than minAppVersionHandlingReadAuth", () => {
+    const res = appVersionHandleReadAuth(MIN_READ_STATUS_PREFERENCES_VERSION)(
+      PREV_APP_VERIONS
+    );
+
+    expect(res).toStrictEqual(false);
+  });
+
+  it("should return true if currentAppVersion is equal to minAppVersionHandlingReadAuth", () => {
+    const res = appVersionHandleReadAuth(MIN_READ_STATUS_PREFERENCES_VERSION)(
+      MIN_READ_STATUS_PREFERENCES_VERSION
+    );
+
+    expect(res).toStrictEqual(true);
+  });
+
+  it("should return true if currentAppVersion is greater than to minAppVersionHandlingReadAuth", () => {
+    const res = appVersionHandleReadAuth(MIN_READ_STATUS_PREFERENCES_VERSION)(
+      NEWER_APP_VERSION
+    );
+
+    expect(res).toStrictEqual(true);
+  });
+});
