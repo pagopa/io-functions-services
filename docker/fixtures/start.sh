8c52cd0c68b053fbb4c2b2ba24b2d8303e2fb895
gquadrati <greta.quadrati@pagopa.it>
2022-10-31 18:54:38 +0100

    fix delay

--- docker/fixtures/start.sh
+++ docker/fixtures/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Sleep 30 sec"
+sleep 30
+
 echo "Start Fixtures"
 node ./dist/index.js
--- docker/functions/start.sh
+++ docker/functions/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Sleep 45 sec"
+sleep 45
+
 echo "Start Azure Function"
 func start --javascript
