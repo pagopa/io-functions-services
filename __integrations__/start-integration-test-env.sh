b0d6549db5157f79b7b04a4a53bc6d0988dbe1e3
gquadrati <greta.quadrati@pagopa.it>
2022-11-03 17:30:27 +0100

    move script to file

---
+++ __integrations__/start-integration-test-env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+yarn install --frozen-lockfile
+yarn generate-env
+
+yarn start azure-cosmosdb-linux-emulator
+
+echo "Wait Cosmos container to setup"
+sleep 15
+
+echo -n "CosmosDB starting..."
+cosmos_started=$(docker logs azure-cosmosdb-linux-emulator | grep -wc Started) || "Not started"
+echo "---> $cosmos_started"
+while [ "$cosmos_started" != "12" ]
+do
+    sleep 5
+    echo -n "."
+    cosmos_started=$(docker logs azure-cosmosdb-linux-emulator | grep -wc Started) || "Not started"
+    echo "-----> $cosmos_started"
+done
+echo "CosmosDB Started"
+
+sleep 30
+yarn start fixtures function testagent
+echo "Env Started"
