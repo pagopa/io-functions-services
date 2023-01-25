1eefd68b22d17ff65e24afa2cd6c39864172fde3
garma00 <giovanniarmaleo00@gmail.com>
2023-01-25 12:12:50 +0100

    remove dangerfile.ts

--- Dangerfile.ts
+++
@@ -1,7 +0,0 @@
-// import custom DangerJS rules
-// see http://danger.systems/js
-// see https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
-// eslint-disable-next-line prettier/prettier
-import checkDangers from "@pagopa/danger-custom-rules";
-
-checkDangers();
