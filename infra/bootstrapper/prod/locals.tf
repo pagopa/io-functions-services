63d8dc9b68955c6290925b81765f6e748e014554
Andrea Grillo <andrea.grillo@pagopa.it>
2025-08-07 15:52:34 +0200

    Add Azure bootstrapper (#322)

---
+++ infra/bootstrapper/prod/locals.tf
@@ -0,0 +1,50 @@
+locals {
+  prefix          = "io"
+  env_short       = "p"
+  location        = "italynorth"
+  domain          = "funcsvc"
+  instance_number = "01"
+  project         = "${local.prefix}-${local.env_short}-itn"
+
+  adgroups = {
+    admins_name = "io-p-adgroup-platform-admins"
+    devs_name   = "io-p-adgroup-svc-developers"
+  }
+
+  runner = {
+    cae_name                = "${local.project}-github-runner-cae-01"
+    cae_resource_group_name = "${local.project}-github-runner-rg-01"
+    secret = {
+      kv_name                = "${local.prefix}-${local.env_short}-kv-common"
+      kv_resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
+    }
+  }
+
+  vnet = {
+    name                = "${local.project}-common-vnet-01"
+    resource_group_name = "${local.project}-common-rg-01"
+  }
+
+  common = {
+    weu_resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
+    itn_resource_group_name = "${local.project}-common-rg-01"
+  }
+
+  tf_storage_account = {
+    name                = "iopitntfst001"
+    resource_group_name = "terraform-state-rg"
+  }
+
+  repository = {
+    name = "io-functions-services"
+  }
+
+  tags = {
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    BusinessUnit   = "App IO"
+    ManagementTeam = "IO Platform"
+    Source         = "https://github.com/pagopa/io-functions-services/blob/main/infra/bootstrapper"
+    CostCenter     = "TS000 - Tecnologia e Servizi"
+  }
+}
---
+++ infra/bootstrapper/prod/main.tf
@@ -0,0 +1,45 @@
+module "bootstrapper" {
+  source  = "pagopa-dx/azure-github-environment-bootstrap/azurerm"
+  version = "~> 3.0"
+
+  environment = {
+    prefix          = local.prefix
+    env_short       = local.env_short
+    location        = local.location
+    domain          = local.domain
+    instance_number = local.instance_number
+  }
+
+  subscription_id = data.azurerm_subscription.current.id
+  tenant_id       = data.azurerm_client_config.current.tenant_id
+
+  entraid_groups = {
+    admins_object_id = data.azuread_group.admins.object_id
+    devs_object_id   = data.azuread_group.developers.object_id
+  }
+
+  github_private_runner = {
+    container_app_environment_id       = data.azurerm_container_app_environment.runner.id
+    container_app_environment_location = data.azurerm_container_app_environment.runner.location
+    key_vault = {
+      name                = local.runner.secret.kv_name
+      resource_group_name = local.runner.secret.kv_resource_group_name
+    }
+  }
+
+  opex_resource_group_id = data.azurerm_resource_group.dashboards.id
+
+  pep_vnet_id                        = data.azurerm_virtual_network.common.id
+  private_dns_zone_resource_group_id = data.azurerm_resource_group.common_weu.id
+
+  repository = {
+    name = local.repository.name
+  }
+
+  terraform_storage_account = {
+    name                = local.tf_storage_account.name
+    resource_group_name = local.tf_storage_account.resource_group_name
+  }
+
+  tags = local.tags
+}
