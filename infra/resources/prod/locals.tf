d4bbb3215b5849c620e426d38a3f76bce68c2ae3
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-08-05 12:13:26 +0200

    initialize tf resource repo

---
+++ infra/resources/prod/locals.tf
@@ -0,0 +1,22 @@
+locals {
+  prefix    = "io"
+  env_short = "p"
+
+  location       = "italynorth"
+  location_short = "itn"
+
+  common_project = "${local.prefix}-${local.env_short}"
+
+  project        = "${local.prefix}-${local.env_short}-${local.location_short}"
+  project_legacy = "${local.prefix}-${local.env_short}"
+
+  tags = {
+    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    Owner          = "IO"
+    ManagementTeam = "IO Platform"
+    Source         = "https://github.com/pagopa/io-functions-services/blob/main/infra/resources/prod"
+  }
+
+}
---
+++ infra/resources/prod/main.tf
@@ -0,0 +1,24 @@
+terraform {
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "iopitntfst001"
+    container_name       = "terraform-state"
+    key                  = "io-functions-services.resources.prod.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "< 5.0.0"
+    }
+    dx = {
+      source  = "pagopa-dx/azure"
+      version = "~> 0.10"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
